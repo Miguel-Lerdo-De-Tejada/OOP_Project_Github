4b6eb58aa10769b4f8c90d74f061701c9b7193a4
Miguel Lerdo de Tejada <61570495+Miguel-Lerdo-De-Tejada@users.noreply.github.com>
2023-06-05 23:12:59 -0600

    Delete GeneralPurposeComponents directory

--- GeneralPurposeComponents/AnimatorShootFields.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AnimatorShootFields : ScriptableObject
-{
-    public static string aim = "Aim";
-    public static string shoot = "Shoot";
-}
--- GeneralPurposeComponents/BulletTarget.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class BulletTarget : MonoBehaviour
-{
-    // Use this component in an object wich is a target for a bullet, in other case, the bullet never do damage to the target.
-}
